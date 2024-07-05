d8dd78c8c0a9db17c74f089e34b421e3599dde7b
Thasso Griebel <thasso.griebel@gmail.com>
2024-07-05 13:49:34 +0200

    Restructure folders and build

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run --rm -ti -v `pwd`:/data dashif-specs $@
+# Here is the command that can be used to debug or develop with the
+# local resources.
+#
+# docker run --rm -ti -v `pwd`:/data -v `pwd`/build-tools/tools:/tools -v `pwd`/data/boilerplate/dashif:/usr/local/lib/python3.12/dist-packages/bikeshed/spec-data/boilerplate/dashif dashif-specs:latest
+#
+
+# Run the docker container and pass all the arguments
+docker run --rm -ti -v `pwd`:/data dashif-specs:latest $@
